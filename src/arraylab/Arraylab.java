e14adc0d339e62205a43ae5cf61da4922513f3af
AbdulMoizChishti <moaiz599@gmail.com>
2021-08-31 19:12:08 +0500

    Arrays Practice

---
+++ src/arraylab/Arraylab.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylab;
+import java.lang.Math;
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class Arraylab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//    int a[] = new int [10];
+//      for (int i=0;i<10;i++){
+//         a[i]=(int)(Math.random()*100);
+//        System.out.println(a[i]);
+//      }
+//    }
+
+
+double m[][] = {
+{ 0*0, 1*0, 2*0, 3*0 },
+{ 0*1, 1*1, 2*1, 3*1 },
+{ 0*2, 1*2, 2*2, 3*2 },
+{ 0*3, 1*3, 2*3, 3*3 }
+};
+int i,j;
+for (i=0;i<4;i++){
+for (j=0;j<4;j++)
+    System.out.print(m[i][j]+"  ");
+
+
+
+
+
+    System.out.println();
+
+    }
+
+
+
+    }
+}
+
